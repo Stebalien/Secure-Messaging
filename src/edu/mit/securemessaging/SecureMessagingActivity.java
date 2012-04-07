bb89bfb1f869996b5f997e62bae5d08d13819c0e
Steven Allen <steven@stebalien.com>
2012-04-07 12:23:42 -0400

    Initial commit.

---
+++ src/edu/mit/securemessaging/SecureMessagingActivity.java
@@ -0,0 +1,13 @@
+package edu.mit.securemessaging;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class SecureMessagingActivity extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
